15f57d5a3b0390d9ee3acd1570f7521c543761b8
T3003 <jtmn.jtmn83@gmail.com>
2024-05-01 23:37:30 -0300

    primeiro commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AmsT4_Carousel.Controllers
+{
+    public class CategoryController : Controller
+    {
+    
+        private ApplicationDataContext _applicationDataContext;
+
+        public CategoryController(ApplicationDataContext applicationDataContext)
+        { _applicationDataContext = applicationDataContext; }
+
+        public IActionResult CategoryList()
+        {
+            var categories = _applicationDataContext.Category.ToList();
+
+            return View(categories);
+        }
+    }
+}
